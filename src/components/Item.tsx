237745c8ea56c4b9c487597bf9ca8b4f603f80c6
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-03 11:33:29 -0300

    handleDelete

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,12 +6,17 @@ import { TodoItem } from "../types/Todos";
 export const Item = () => {
     const [todos, setTodos] = useState<TodoItem[]>(Todos);
 
+    const handleDelete = (id: number) => {
+        const items = todos.filter(index => index.id !== id);
+        setTodos(items);        
+    }
+
     return (
         <div>
             {todos.map(item => (
                 <div className="flex justify-between border-b px-3 p-2 bg-slate-500/5 hover:bg-green-100 duration-300">
                     <h1 className="font-medium text-slate-600 text-lg">{item.label}</h1>
-                    <span className="text-blue-500"><Trash2 className="hover:stroke-red-700 transition-all duration-300"/></span>
+                    <span className="text-blue-500" onClick={() => handleDelete(item.id)}><Trash2 className="hover:stroke-red-700 transition-all duration-300"/></span>
                 </div>
             ))}
         </div>
